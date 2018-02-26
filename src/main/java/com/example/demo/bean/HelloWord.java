11a81fd4d0a63990c1a2ab8010feeede85bd24ca
Artur Veiga <arturveiga_@hotmail.com>
2018-02-26 15:47:58 -0300

    org

--- src/main/java/com/example/demo/bean/HelloWord.java
+++ src/main/java/com/example/demo/bean/HelloWord.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.bean;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/com/example/demo/config/ExampleConfigurationProvider.java
+++ src/main/java/com/example/demo/config/ExampleConfigurationProvider.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
 import org.ocpsoft.rewrite.config.Configuration;
