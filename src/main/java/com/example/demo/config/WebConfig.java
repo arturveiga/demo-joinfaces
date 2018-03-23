bfc34677c6d3a8d331dafdfbc7fcb53a9aadf71c
Artur Veiga <arturveiga_@hotmail.com>
2018-03-23 15:38:41 -0300

    Add materialize

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -2,17 +2,20 @@ package com.example.demo.config;
 
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * Created by Admin on 22/03/2018.
  */
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements ServletContextInitializer {
 
     @Bean
     public FilterRegistrationBean prettyFilter() {
@@ -21,5 +24,10 @@ public class WebConfig {
                 DispatcherType.ERROR);
         registration .addUrlPatterns("/*");
         return registration ;
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setAttribute("com.ocpsoft.pretty.CONFIG_FILES",new PrettyConfigCustom());
     }
 }
